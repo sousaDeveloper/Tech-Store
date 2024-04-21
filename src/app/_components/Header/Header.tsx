2c5ddeb0de5a2fa324f07bcbaf3e06754998160b
Matheus Sousa <devlpsousa@gmail.com>
2024-04-21 18:10:25 -0300

    feat: add useRouter in buttons of header

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -20,8 +20,21 @@ import {
 import ButtonMenu from "./ButtonMenu/ButtonMenu";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Header() {
+  const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  const handleRouterClick = (path: string) => {
+    router.push(path);
+    setIsMenuOpen(false);
+    setTimeout(() => {
+      return setIsMenuOpen(true);
+    }, 1000);
+  };
+
   const { status, data } = useSession();
 
   const handleLoginClick = async () => {
@@ -40,56 +53,58 @@ export default function Header() {
             <MenuIcon size={21} />
           </Button>
         </SheetTrigger>
-        <SheetContent side="left" className="p-0 pt-[1.3rem]">
-          <SheetHeader className="w-full border-b pb-3 pl-3">
-            <SheetTitle>Menu</SheetTitle>
-          </SheetHeader>
+        {isMenuOpen && (
+          <SheetContent side="left" className="p-0 pt-[1.3rem]">
+            <SheetHeader className="w-full border-b pb-3 pl-3">
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
 
-          {status === "authenticated" && data?.user && (
-            <div className="flex items-center gap-2 p-2 pb-0">
-              <Avatar>
-                <AvatarFallback>
-                  {data.user.name?.[0].toUpperCase()}
-                </AvatarFallback>
-                {data.user.image && <AvatarImage src={data.user.image} />}
-              </Avatar>
+            {status === "authenticated" && data?.user && (
+              <div className="flex items-center gap-2 p-2 pb-0">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
 
-              <div className="flex flex-col">
-                <p>{data.user.name}</p>
-                <p className="text-sm opacity-65">Boas compras!</p>
+                <div className="flex flex-col">
+                  <p>{data.user.name}</p>
+                  <p className="text-sm opacity-65">Boas compras!</p>
+                </div>
               </div>
+            )}
+
+            <div className="mx-2 mt-5 flex flex-col gap-2">
+              {status === "unauthenticated" && (
+                <ButtonMenu onClick={handleLoginClick}>
+                  <LogInIcon size={16} /> Fazer Login
+                </ButtonMenu>
+              )}
+
+              <ButtonMenu onClick={() => handleRouterClick("/")}>
+                <HomeIcon size={16} />
+                <p>Início</p>
+              </ButtonMenu>
+
+              <ButtonMenu onClick={() => handleRouterClick("/catalog")}>
+                <ListOrderedIcon size={16} />
+                <p>Catálogo</p>
+              </ButtonMenu>
+
+              <ButtonMenu>
+                <PercentIcon size={16} />
+                Ofertas
+              </ButtonMenu>
+
+              {status === "authenticated" && (
+                <ButtonMenu onClick={handleLogoutClick}>
+                  <LogOutIcon size={16} /> Sair da Conta
+                </ButtonMenu>
+              )}
             </div>
-          )}
-
-          <div className="mx-2 mt-5 flex flex-col gap-2">
-            {status === "unauthenticated" && (
-              <ButtonMenu onClick={handleLoginClick}>
-                <LogInIcon size={16} /> Fazer Login
-              </ButtonMenu>
-            )}
-
-            <ButtonMenu>
-              <HomeIcon size={16} />
-              Início
-            </ButtonMenu>
-
-            <ButtonMenu>
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </ButtonMenu>
-
-            <ButtonMenu>
-              <PercentIcon size={16} />
-              Ofertas
-            </ButtonMenu>
-
-            {status === "authenticated" && (
-              <ButtonMenu onClick={handleLogoutClick}>
-                <LogOutIcon size={16} /> Sair da Conta
-              </ButtonMenu>
-            )}
-          </div>
-        </SheetContent>
+          </SheetContent>
+        )}
       </Sheet>
 
       <h1 className="bg-gradient-to-r from-[#5033C3] to-purple-600 bg-clip-text text-xl font-semibold text-transparent">
