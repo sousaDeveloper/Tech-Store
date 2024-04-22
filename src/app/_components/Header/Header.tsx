780250ab608094d401e7176d965e450010ffc87b
Matheus Sousa <devlpsousa@gmail.com>
2024-04-22 10:10:23 -0300

    fix: ensure that the menu closes when you click on an item in the sidebar

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -1,12 +1,13 @@
 "use client";
+
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetHeader,
   SheetTitle,
@@ -26,14 +27,9 @@ import ButtonMenu from "./ButtonMenu/ButtonMenu";
 
 export default function Header() {
   const router = useRouter();
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
   const handleRouterClick = (path: string) => {
     router.push(path);
-    setIsMenuOpen(false);
-    setTimeout(() => {
-      return setIsMenuOpen(true);
-    }, 1000);
   };
 
   const { status, data } = useSession();
@@ -60,61 +56,72 @@ export default function Header() {
             <MenuIcon size={21} />
           </Button>
         </SheetTrigger>
-        {isMenuOpen && (
-          <SheetContent side="left" className="p-0 pt-[1.3rem]">
-            <SheetHeader className="w-full border-b pb-3 pl-3">
-              <SheetTitle>Menu</SheetTitle>
-            </SheetHeader>
+        <SheetContent side="left" className="p-0 pt-[1.3rem]">
+          <SheetHeader className="w-full border-b pb-3 pl-3">
+            <SheetTitle>Menu</SheetTitle>
+          </SheetHeader>
 
-            {status === "authenticated" && data?.user && (
-              <div className="flex items-center gap-2 p-2 pb-0">
-                <Avatar>
-                  <AvatarFallback>
-                    {data.user.name?.[0].toUpperCase()}
-                  </AvatarFallback>
-                  {data.user.image && <AvatarImage src={data.user.image} />}
-                </Avatar>
+          {status === "authenticated" && data?.user && (
+            <div className="flex items-center gap-2 p-2 pb-0">
+              <Avatar>
+                <AvatarFallback>
+                  {data.user.name?.[0].toUpperCase()}
+                </AvatarFallback>
+                {data.user.image && <AvatarImage src={data.user.image} />}
+              </Avatar>
 
-                <div className="flex flex-col">
-                  <p>{data.user.name}</p>
-                  <p className="text-sm opacity-65">Boas compras!</p>
-                </div>
+              <div className="flex flex-col">
+                <p>{data.user.name}</p>
+                <p className="text-sm opacity-65">Boas compras!</p>
               </div>
-            )}
+            </div>
+          )}
 
-            <div className="mx-2 mt-5 flex flex-col gap-2">
-              {status === "unauthenticated" && (
+          <div className="mx-2 mt-5 flex flex-col gap-2">
+            {status === "unauthenticated" && (
+              <SheetClose>
                 <ButtonMenu onClick={handleLoginClick}>
                   <LogInIcon size={16} /> Fazer Login
                 </ButtonMenu>
-              )}
+              </SheetClose>
+            )}
 
+            <SheetClose>
               <ButtonMenu onClick={() => handleRouterClick("/")}>
                 <HomeIcon size={16} />
                 <p>Início</p>
               </ButtonMenu>
+            </SheetClose>
 
+            <SheetClose>
               <ButtonMenu onClick={() => handleRouterClick("/catalog")}>
                 <ListOrderedIcon size={16} />
                 <p>Catálogo</p>
               </ButtonMenu>
+            </SheetClose>
 
+            <SheetClose>
               <ButtonMenu>
                 <PercentIcon size={16} />
                 Ofertas
               </ButtonMenu>
+            </SheetClose>
 
-              {status === "authenticated" && (
+            {status === "authenticated" && (
+              <SheetClose>
                 <ButtonMenu onClick={handleLogoutClick}>
                   <LogOutIcon size={16} /> Sair da Conta
                 </ButtonMenu>
-              )}
-            </div>
-          </SheetContent>
-        )}
+              </SheetClose>
+            )}
+          </div>
+        </SheetContent>
       </Sheet>
 
-      <h1 className="bg-gradient-to-r from-[#5033C3] to-purple-600 bg-clip-text text-xl font-semibold text-transparent">
+      <h1
+        className="bg-gradient-to-r from-[#5033C3] to-purple-600 bg-clip-text text-xl font-semibold text-transparent"
+        onClick={() => handleRouterClick("/")}
+      >
         Tech Store
       </h1>
 
