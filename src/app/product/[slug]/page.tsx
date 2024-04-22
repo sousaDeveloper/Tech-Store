ea1a0d1bf2bfa0b6c001ecb12f6a8de87e5680ed
Matheus Sousa <devlpsousa@gmail.com>
2024-04-22 14:20:08 -0300

    feat: add ProductImages component

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import ButtonBack from "@/app/catalog/components/button-back";
 import { prismaClient } from "@/lib/prisma";
-import Image from "next/image";
+import ProductImages from "../components/product-images";
+import ProductInfo from "../components/product-info";
 
 interface ProductDetailsPageProps {
   params: {
@@ -20,19 +20,9 @@ export default async function ProductDetailsPage({
   if (!product) return null;
 
   return (
-    <div className="bg-accent">
-      <div className="mt-[5.1rem] h-full bg-accent">
-        <div className="p-5">
-          <ButtonBack />
-        </div>
-        <Image
-          src={product.imageUrls[0]}
-          alt={slug}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto w-full"
-        />
+    <div>
+      <div className="mt-[5rem] h-full">
+        <ProductImages imageUrls={product.imageUrls} name={product.name} />
       </div>
     </div>
   );
---
+++ src/app/product/components/product-images.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import ButtonBack from "@/app/catalog/components/button-back";
+
+interface ProductImagesProps {
+  name: string;
+  imageUrls: string[];
+}
+
+const ProductImages = ({ name, imageUrls }: ProductImagesProps) => {
+  const [currentImage, setCurrentImage] = useState(imageUrls[0]);
+
+  const handleImageClick = (imageUrl: string) => {
+    return setCurrentImage(imageUrl);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex h-[380px] w-full items-center justify-center bg-accent">
+        <div className="absolute left-[0.05rem] top-20 p-5 pb-0">
+          <ButtonBack />
+        </div>
+        <Image
+          src={currentImage}
+          alt={name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
+      <div className="mt-8 grid grid-cols-4 gap-4 px-5">
+        {imageUrls.map((imageUrl, index) => (
+          <button
+            key={index}
+            onClick={() => handleImageClick(imageUrl)}
+            className={`flex h-20 cursor-pointer items-center justify-center rounded-lg bg-accent ${currentImage === imageUrl && "border-2 border-primary"}`}
+          >
+            <Image
+              src={imageUrl}
+              alt={name}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-auto max-h-[70%] w-auto max-w-[80%]"
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
