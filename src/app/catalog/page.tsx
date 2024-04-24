ad0400d7ff65062356b52d2ee70be6dbdd761eb4
Matheus Sousa <devlpsousa@gmail.com>
2024-04-24 09:52:28 -0300

    feat: add ButtonBack component in catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -4,21 +4,25 @@ import { prismaClient } from "@/lib/prisma";
 
 import { Badge } from "@/components/ui/badge";
 import CategoryList from "./components/category-list";
+import ButtonBack from "./components/button-back";
 
 export default async function CatalogPage() {
   const categories = await prismaClient.category.findMany({});
 
   return (
     <div className="mt-20 p-5">
-      <Badge
-        className="flex w-[7rem] gap-1 border-primary uppercase"
-        variant="outline"
-        data-aos="fade-right"
-        data-aos-duration="1000"
-      >
-        <ShapesIcon />
-        Catálogo
-      </Badge>
+      <div className="flex justify-between">
+        <ButtonBack />
+        <Badge
+          className="flex w-[7rem] gap-1 border-primary uppercase"
+          variant="outline"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+        >
+          <ShapesIcon />
+          Catálogo
+        </Badge>
+      </div>
 
       <div
         className="mt-6 flex flex-wrap justify-center gap-8"
@@ -26,7 +30,7 @@ export default async function CatalogPage() {
         data-aos-duration="1000"
       >
         {categories.map((category) => (
-          <CategoryList category={category} key={category.id}/>
+          <CategoryList category={category} key={category.id} />
         ))}
       </div>
     </div>
