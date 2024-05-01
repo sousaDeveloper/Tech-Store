0892e75b205aeb1510db3cbd46e474303106c41a
Matheus Sousa <devlpsousa@gmail.com>
2024-05-01 10:53:57 -0300

    feat: ensure products are fetched on products page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,4 +1,8 @@
-const ProductsPage = () => {
+import { prismaClient } from "@/lib/prisma";
+
+const ProductsPage = async () => {
+  const products = await prismaClient.product.findMany({});
+
   return <h1>Produtos</h1>;
 };
 
