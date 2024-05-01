e342fec8efce5a6fca2be69790ed9257caf18e36
Matheus Sousa <devlpsousa@gmail.com>
2024-05-01 11:27:29 -0300

    feat: ensure andd button and products found quantity are shown in products page

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { Badge } from "@/components/ui/badge";
+import { LayoutDashboardIcon } from "lucide-react";
+
 const DashboardPage = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="w-full p-8">
+      <Badge className="flex h-fit w-fit items-center gap-1 border-2 border-primary bg-accent uppercase">
+        <LayoutDashboardIcon size={16} />
+        Dashboard
+      </Badge>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,9 +1,29 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { prismaClient } from "@/lib/prisma";
+import { PackageIcon, PlusIcon } from "lucide-react";
+
+import "./button.css";
 
 const ProductsPage = async () => {
   const products = await prismaClient.product.findMany({});
 
-  return <h1>Produtos</h1>;
+  return (
+    <div className="flex w-full flex-col gap-10 p-8">
+      {" "}
+      <Badge className="flex h-fit w-fit items-center gap-1 border-2 border-primary bg-accent uppercase">
+        <PackageIcon size={18} />
+        Produtos
+      </Badge>
+      <div className="flex items-center justify-between">
+        <p className="text-lg">Produtos encontrados: {products.length}</p>
+        <Button className="buttonShadow flex items-center gap-2 transition-all duration-300">
+          <PlusIcon size={18} />
+          Adicionar produto
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsPage;
